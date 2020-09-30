bbd2be63e49de8dbb992789ab8d0dbf472953272
dannyelgjl <dannyelgjl@gmail.com>
2020-09-30 14:04:55 -0300

    stylization Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,119 @@
 import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-// import { Container } from './styles';
+import { ProductList } from "./styles";
 
 function Home() {
   return (
-    <div>
-      <h1>teste</h1>
-    </div>
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+          alt="Tennis"
+        />
+
+        <strong>Tênis muito daora</strong>
+        <span>R$130,50</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+          alt="Tennis"
+        />
+
+        <strong>Tênis muito daora</strong>
+        <span>R$130,50</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+          alt="Tennis"
+        />
+
+        <strong>Tênis muito daora</strong>
+        <span>R$130,50</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+          alt="Tennis"
+        />
+
+        <strong>Tênis muito daora</strong>
+        <span>R$130,50</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+          alt="Tennis"
+        />
+
+        <strong>Tênis muito daora</strong>
+        <span>R$130,50</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+          alt="Tennis"
+        />
+
+        <strong>Tênis muito daora</strong>
+        <span>R$130,50</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
 
