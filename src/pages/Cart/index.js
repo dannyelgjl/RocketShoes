176a6f970bf631d7e7189b5fe41ccd809b3b3794
dannyelgjl <dannyelgjl@gmail.com>
2020-09-30 14:36:55 -0300

    Cart page styling

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,73 @@
 import React from "react";
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from "react-icons/md";
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from "./styles";
 
 function Cart() {
-  return <div />;
+  return (
+    <Container style={{ background: "#fff", padding: 30, borderRadius: 5 }}>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-feminino/72/NQQ-4634-172/NQQ-4634-172_zoom1.jpg?ts=1599759001&"
+                alt="Tennis"
+              />
+            </td>
+
+            <td>
+              <strong>Tênis top dms</strong>
+              <span>R$ 130</span>
+            </td>
+
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$250</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$180,30</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 export default Cart;
